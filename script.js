76c3b075632d04728783c5267ed5a9aac2dbe22a
Sandeep <98701264+5andeeep@users.noreply.github.com>
2023-05-24 17:44:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 //your JS code here. If required.
 
-const container = document.getElementsByClassName("container")[0];
-container.className = "container";
 
-const div = document.createElement("div");
-div.id = "line";
-container.append(div);
