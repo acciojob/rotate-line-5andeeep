885ebc2cc5d69c363e3533f6a98a26d488e3d892
Sandeep <newsandeep03@gmail.com>
2023-05-24 16:22:21 +0530

    ans added

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const container = document.getElementsByClassName("container")[0];
+container.className = "container";
+
+const div = document.createElement("div");
+div.id = "line";
+container.append(div);
